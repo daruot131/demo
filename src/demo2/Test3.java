3cde3e7b14545abafc0d7819022e180f46f3b691
tuananh <trantuananh.9.9.2014@gmail.com>
2022-08-03 17:07:26 +0700

    firt commit

---
+++ src/demo2/Test3.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package demo2;
+
+/**
+ * @author tuananh
+ *
+ */
+public class Test3 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("bbbbbbb");
+        // TODO Auto-generated method stub
+
+    }
+
+}
