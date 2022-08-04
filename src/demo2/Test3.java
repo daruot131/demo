dd91d4dbf79c127638e5b855934540578b658375
tuananh <trantuananh.9.9.2014@gmail.com>
2022-08-04 09:23:36 +0700

    p

--- src/demo2/Test3.java
+++ src/demo2/Test3.java
@@ -15,6 +15,7 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("bbbbbbb");
         System.out.println("ooooooo");
+        System.out.println("ppppppp");
         // TODO Auto-generated method stub
 
     }
